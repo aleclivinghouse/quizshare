adbc49f26c9a7eda824363d22dc9395baccd5f57
aleclivinghouse <aleclivinghouse@yahoo.com>
2019-02-17 18:47:21 -0800

    undefined issue fixed

--- client/src/components/post/CommentFeed.js
+++ client/src/components/post/CommentFeed.js
@@ -5,11 +5,12 @@ import CommentItem from './CommentItem';
 class CommentFeed extends Component {
   render() {
     const { comments, postId } = this.props;
-
+    if(comments !== undefined){
     return comments.map(comment => (
       <CommentItem key={comment._id} comment={comment} postId={postId} />
     ));
   }
+}
 }
 
 export default CommentFeed;
