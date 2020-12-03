476c6a5363dd0f62cfe1cdbf80337ef8079620f6
aleclivinghouse <aleclivinghouse@yahoo.com>
2020-12-03 12:05:35 -0800

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb+srv://@mern-social.nzwc6.mongodb.net/mern-social?retryWrites=true&w=majority
-',
+  mongoURI: 'mongodb+srv://@mern-social.nzwc6.mongodb.net/mern-social?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 };
